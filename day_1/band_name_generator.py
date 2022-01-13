d5924d8d3f0e692d7254311a98798d1271640f8d
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-13 18:11:24 +0300

    Make some readjustments

--- day_1/band_name_generator.py
+++ day_1/band_name_generator.py
@@ -3,4 +3,4 @@ print("Welcome to the band name generator")
 city_name = input("What city did you grow up in?\n")
 pet_name = input("What's the name of your pet?\n")
 
-print("Your band name is " + city_name+pet_name)
+print("Your band name is " +city_name + " " +pet_name)
