c1f608518691a76f743f2b13d5a0d944e09c3c59
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-13 19:01:46 +0300

    Type conversions

---
+++ day_2/data_types.py
@@ -0,0 +1,19 @@
+# strings
+print("Helloo"[3]);
+
+# Integers
+print(123 + 435)
+
+print(123_476_98)
+
+# Floats
+print(3.256)
+
+# Booleans
+# True / False
+
+num_char = len(input("What is your name?"))
+
+print(type(num_char))
+
+print("Your name has " + str(num_char) + " characters")
