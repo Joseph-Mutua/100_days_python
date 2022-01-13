462bd74ce51abae529c4cbad9edc021c78913fa0
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-13 19:12:02 +0300

    Data types

--- day_2/data_types.py
+++ day_2/data_types.py
@@ -4,16 +4,27 @@ print("Helloo"[3]);
 # Integers
 print(123 + 435)
 
-print(123_476_98)
+# print(123_476_98)
 
-# Floats
-print(3.256)
+# # Floats
+# print(3.256)
 
-# Booleans
-# True / False
+# # Booleans
+# # True / False
 
-num_char = len(input("What is your name?"))
+# num_char = len(input("What is your name?"))
 
-print(type(num_char))
+# print(type(num_char))
 
-print("Your name has " + str(num_char) + " characters")
+# print("Your name has " + str(num_char) + " characters")
+
+two_digit_num = input("Type a 2 digit number\n")
+
+print(type(two_digit_num))
+
+first_digit=two_digit_num[0]
+second_digit=two_digit_num[1]
+
+print(float(first_digit) + float(second_digit))
+
+print(int(first_digit) + int(second_digit))
