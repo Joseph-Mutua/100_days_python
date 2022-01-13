a5bdaa72cbf58047a312a017460e7950535d3d73
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-13 20:43:24 +0300

    Number manipulation and F strings

--- day_2/data_types.py
+++ day_2/data_types.py
@@ -1,46 +1,57 @@
-# strings
-print("Helloo"[3])
+# # strings
+# print("Helloo"[3])
 
-# Integers
-print(123 + 435)
+# # Integers
+# print(123 + 435)
 
-print(123_476_98)
+# print(123_476_98)
 
-# Floats
-print(3.256)
+# # Floats
+# print(3.256)
 
-# Booleans
-# True / False
+# # Booleans
+# # True / False
 
-num_char = len(input("What is your name?"))
+# num_char = len(input("What is your name?"))
 
-print(type(num_char))
+# print(type(num_char))
 
-print("Your name has " + str(num_char) + " characters")
+# print("Your name has " + str(num_char) + " characters")
 
-two_digit_num = input("Type a 2 digit number\n")
+# two_digit_num = input("Type a 2 digit number\n")
 
-print(type(two_digit_num))
+# print(type(two_digit_num))
 
-first_digit=two_digit_num[0]
-second_digit=two_digit_num[1]
+# first_digit=two_digit_num[0]
+# second_digit=two_digit_num[1]
 
-print(float(first_digit) + float(second_digit))
+# print(float(first_digit) + float(second_digit))
 
-print(int(first_digit) + int(second_digit))
+# print(int(first_digit) + int(second_digit))
 
-# Mathematical operations
-# Division always brings floating point results
-print(6 / 3)
+# # Mathematical operations
+# # Division always brings floating point results
+# print(6 / 3)
 
-# Power
-print(2**4)
+# # Power
+# print(2**4)
 
-height = input("Enter your height in metres: \n")
-weight = input("Enter your weight in kg: \n")
+# height = input("Enter your height in metres: \n")
+# weight = input("Enter your weight in kg: \n")
 
+# bmi_index = int(((float(weight) / (float(height)**2))))
 
+# print("your BMI index is: " + str(bmi_index))
 
-bmi_index = int(((float(weight) / (float(height)**2))))
+# Number manipulation and F strings
+print(round(8 / 3, 6))
 
-print("your BMI index is: " + str(bmi_index))
+# Floor
+print(8 // 3)
+
+result = 4 / 2
+result /= 2
+print(result)
+
+# F strings
+print(f"Your score is {result}")
