341439baa53977323a0f91346dac195f778e87e8
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-18 11:14:10 +0300

    Banker roulette

--- day_4/randomization.py
+++ day_4/randomization.py
@@ -1,7 +1,23 @@
 import random
 
-random_integer = random.randint(1, 10)
-random_float = random.random()
+# random_integer = random.randint(1, 10)
+# random_float = random.random()
 
+# print(int(random_float * 2))
 
-print(random_integer)
+# A virtual coin toss program
+# str_arr = ["Heads", "Tails"]
+# random_int = int(random.random() * 2)
+
+# random_choice = str_arr[random_int]
+# print(random_choice)
+
+# Banker Roulette
+names_string = input("Give me everybody's names, separated by a comma.\n")
+names_list = names_string.split(",")
+
+random_int = int(random.random() * len(names_list))
+
+random_name = names_list[random_int]
+
+print(random_name)
