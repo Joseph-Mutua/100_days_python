e0deab12ec9353169795e2dcfd41d4107ac83056
Joseph Mutua <Mutuaj793@gmail.com>
2022-02-21 18:06:24 +0300

    Adding even numbers

--- day_5/loops.py
+++ day_5/loops.py
@@ -3,9 +3,9 @@
 
 # for fruit in fruits:
 #     print(fruit + " Pie")
-        
-        
-# # Average height calculator  
+
+
+# # Average height calculator
 # student_heights = input("Input a list of student heights\n").split()
 
 # sum_of_heights = 0
@@ -13,7 +13,7 @@
 # for n in range(0, len(student_heights)):
 #     student_heights[n] = int(student_heights[n])
 #     sum_of_heights += student_heights[n]
-    
+
 #  # Can also use the sum function
 # sum_of_heights = sum(student_heights)
 
@@ -22,15 +22,24 @@
 
 # # Write a program that calculates the highest score from a list of scores
 # # Dont use the max/min functions
-student_scores = input("Input a list of student scores\n").split()
+# student_scores = input("Input a list of student scores\n").split()
 
-for n in range(0, len(student_scores)):
-    student_scores[n] = int(student_scores[n])
-    
-max_score = 0
+# for n in range(0, len(student_scores)):
+#     student_scores[n] = int(student_scores[n])
 
-for score in student_scores:
-    if score > max_score:
-        max_score = score
+# max_score = 0
 
-print(max_score)
+# for score in student_scores:
+#     if score > max_score:
+#         max_score = score
+
+# print(max_score)
+
+# Write a program that calculates the sum of even numbers from 1 to 100 including 100
+
+sum_of_nums = 0
+for num in range(1, 101):
+    if num % 2 == 0:
+        sum_of_nums += num
+
+print(sum_of_nums)
