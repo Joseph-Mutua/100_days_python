d371a6e36da21030104f134bc1dc53b48898c219
Joseph Mutua <Mutuaj793@gmail.com>
2022-02-21 17:40:53 +0300

    Average heights calculator

--- day_5/loops.py
+++ day_5/loops.py
@@ -2,4 +2,17 @@
 fruits = ["Apple", "Peach", "Pear"]
 
 for fruit in fruits:
-    print(fruit)
+    print(fruit + " Pie")
+        
+        
+# Average height calculator  
+student_heights = input("Input a list of student heights\n").split()
+
+sum_of_heights = 0
+
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+    sum_of_heights += student_heights[n]
+    
+avg_height = int(sum_of_heights/len(student_heights))
+print(avg_height)
