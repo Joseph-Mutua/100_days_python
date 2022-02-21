667a37fd7c29a2d3e280d79f9ac16a7ba12273de
Joseph Mutua <Mutuaj793@gmail.com>
2022-02-21 18:20:02 +0300

    fizzBuzz

--- day_5/loops.py
+++ day_5/loops.py
@@ -35,11 +35,24 @@
 
 # print(max_score)
 
-# Write a program that calculates the sum of even numbers from 1 to 100 including 100
+# # Write a program that calculates the sum of even numbers from 1 to 100 including 100
 
-sum_of_nums = 0
+# sum_of_nums = 0
+# for num in range(1, 101):
+#     if num % 2 == 0:
+#         sum_of_nums += num
+
+# print(sum_of_nums)
+
+
+# Write a program that automatically prints the solution tot he FizzBuzz game
+
 for num in range(1, 101):
-    if num % 2 == 0:
-        sum_of_nums += num
-
-print(sum_of_nums)
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
