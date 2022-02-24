115027070675125c29d19af48a6e494582029bf2
Joseph Mutua <Mutuaj793@gmail.com>
2022-02-24 14:00:28 +0300

    Finish up the caesar cipher

--- day_8/cipher.py
+++ day_8/cipher.py
@@ -2,7 +2,7 @@
 #     print(f"Good Morning {name}")
 #     print(f"Hope you're having a beautiful {weekday} morning")
 #     print(f"Our servers show you're playing from {location}")
-    
+
 
 # greet(location="Juja",name="Mutua", weekday="Friday",)
 
@@ -21,38 +21,64 @@
 # ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 # #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+from art import logo
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def encrypt(text, shift, direction):
-    if direction == "encode":
-        new_text = []
-        for char in text:
+def encrypt(text, shift):
+    new_text = []
+    for char in text:
+        if char in alphabet:
             index_in_alphabet = alphabet.index(char)
-            char = alphabet[index_in_alphabet + shift]
-            new_text.append(char)
-            
-        new_str=""
-        for char in new_text:
-            new_str += char
-            
-        print(new_str)
-        
-    elif direction == "decode":
-        new_text = []
-        for char in text:
-            index_in_alphabet = alphabet.index(char)
-            char = alphabet[index_in_alphabet - shift]
-            new_text.append(char)
+            new_char = alphabet[index_in_alphabet + shift]
+            new_text.append(new_char)
+        else:
+            new_text += char
 
         new_str = ""
         for char in new_text:
             new_str += char
 
-        print(new_str)
-        
-encrypt(text,shift, direction)
+    print(f"The encoded string is {new_str}")
+
+
+def decrypt(text, shift):
+    new_text = []
+    for char in text:
+        if char in alphabet:
+            index_in_alphabet = alphabet.index(char)
+            new_char = alphabet[index_in_alphabet - shift]
+            new_text.append(new_char)
+        else:    
+            new_text += char
+
+        new_str = ""
+        for char in new_text:
+            new_str += char
+
+    print(f"The decoded string is {new_str}")
+
+
+def caesar_cipher(text, shift, direction):
+    shift = shift % 26
+    if direction == "encode":
+        encrypt(text, shift)
+    elif direction == "decode":
+        decrypt(text, shift)
+
+
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar_cipher(text, shift, direction)
+    result = input("Type 'yes' if you want to continue, otherwise type 'no' \n")
+
+    if result == "no":
+        should_continue = False
+        print("Goodbye")
