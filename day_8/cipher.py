7836fa56bfc681d95832f50a9d092a743ef6091d
Joseph Mutua <Mutuaj793@gmail.com>
2022-02-24 12:03:33 +0300

    Positional commits

---
+++ day_8/cipher.py
@@ -0,0 +1,8 @@
+def greet(name, weekday, location):
+    print(f"Good Morning {name}")
+    print(f"Hope you're having a beautiful {weekday} morning")
+    print(f"Our servers show you're playing from {location}")
+    
+
+greet(location="Juja",name="Mutua", weekday="Friday",)
+
