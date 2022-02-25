24e86a30b586c90e1f20dddaba1f914571a2e9e4
Joseph Mutua <Mutuaj793@gmail.com>
2022-02-25 12:35:47 +0300

    Travel log

--- day_9/dictionaries.py
+++ day_9/dictionaries.py
@@ -15,9 +15,8 @@ programming_dictionary["Bug"] = "A roach that has invaded your laptop"
 # Loop through a dictionary
 for key in programming_dictionary:
     print(key)
-    
-    
-    
+
+
 # First Coding Project
 student_scores = {
     "Harry": 81,
@@ -28,10 +27,10 @@ student_scores = {
 }
 # 🚨 Don't change the code above 👆
 
-#TODO-1: Create an empty dictionary called student_grades.
+# TODO-1: Create an empty dictionary called student_grades.
 student_grades = {}
 
-#TODO-2: Write your code below to add the grades to student_grades.👇
+# TODO-2: Write your code below to add the grades to student_grades.👇
 for key in student_scores:
     if student_scores[key] >= 91:
         student_grades[key] = "Outstanding"
@@ -44,3 +43,51 @@ for key in student_scores:
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
+
+
+# Nesting Lists and Dictionaries
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": {"cities_visited": ["Hamburg", "Berlin", "Stuttgart"]},
+}
+
+# Nesting Dictionaries inside a list
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited":  ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    }, {
+        "country": "Germany",
+        "cities_visited":  ["Hamburg", "Berlin", "Stuttgart"],
+        "total_visits": 5
+    }
+]
+
+
+# Dictionary in List
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+# 🚨 Do NOT change the code above
+
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log. 👇
+
+
+def add_new_country(country, visits, cities):
+    travel_log.append({"country": country, "visits": visits, "cities": cities})
+
+
+# 🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
