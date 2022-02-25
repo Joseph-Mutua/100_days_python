d58acdce6a4e66eb2814754aff133487898a7407
Joseph Mutua <Mutuaj793@gmail.com>
2022-02-25 12:02:15 +0300

    Dictionary basics

---
+++ day_9/dictionaries.py
@@ -0,0 +1,38 @@
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+                          "Function": "A piece of code that you can easily call over and over again."}
+
+print(programming_dictionary["Bug"])
+
+# Adding new items to dictionary
+programming_dictionary["Loop"] = "Piece of code that runs repeatedly"
+
+# Edit an item in a dictionary
+programming_dictionary["Bug"] = "A roach that has invaded your laptop"
+
+# print(programming_dictionary)
+
+
+# Loop through a dictionary
+for key in programming_dictionary:
+    print(key)
+    
+    
+    
+# First Coding Project
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
