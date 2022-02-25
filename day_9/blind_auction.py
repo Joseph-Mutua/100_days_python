457051bb6c273f5dc1db79f478349c7c728c6250
Joseph Mutua <Mutuaj793@gmail.com>
2022-02-25 13:20:22 +0300

    Refactor

--- day_9/blind_auction.py
+++ day_9/blind_auction.py
@@ -1,3 +1,5 @@
+
+# Secret bid program
 from art import logo
 print(logo)
 
@@ -7,7 +9,7 @@ while continue_bid == "yes":
     name = input("Please type your name\n")
     bid_price = int(input("Please type your bid price\n"))
 
-    bids[name] = bid_price,
+    bids[name] = bid_price
 
     continue_bid = input(
         "Are there any other users who want to bid? Type 'yes' or 'no'\n").lower()
