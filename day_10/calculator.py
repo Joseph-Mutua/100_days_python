3ab48fac8d4f26fe7d758f3c2d7f2adfc9cd7f19
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-01 13:45:29 +0300

    Finish up basic functionality for calculator

--- day_10/calculator.py
+++ day_10/calculator.py
@@ -1,34 +1,37 @@
-# # Functions with Outputs
+from art import logo
 
-# def format_name(f_name, l_name):
-#     formatted_f_name = f_name.title()
-#     formatted_l_name = l_name.title()
-#     return f"{formatted_f_name} {formatted_l_name}"
-# print(format_name("mutua", "joseph"))
+print(logo)
+
+# Calculator --> Add, Subtract, Multiply, Divide
 
 
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+# Add
+def add(num1, num2):
+    return num1 + num2
 
 
-def days_in_month(year, month):
-    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_leap(year) and month == 2:
-       return 29
-    else:
-        return month_days[month - 1]
-    
-#🚨 Do NOT change any of the code below
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
-days = days_in_month(year, month)
-print(days)
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+
+
+num1 = float(input("Type the first operand:\n"))
+num2 = float(input("Type the second operand:\n"))
+
+for operation in operations:
+    print(operation)
+
+operation_symbol = input("Please choose the operation from the line above:\n")
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
