c7fdd66bc89dbf4b9fa90711779917249f87678c
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-16 11:30:47 +0300

    Oop basics

---
+++ day_16/oop.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+mutua = Turtle()
+
+print(mutua)
+mutua.shape("turtle")
+mutua.color("coral")
+mutua.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
